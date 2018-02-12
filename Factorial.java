21072f673fa234b6a27399f197f25a7749b495de
jmoranmartinez <tgujmora@gmail.com>
2018-02-11 23:50:58 -0600

    Contenido para factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,1 +1,10 @@
-# Factorial
+class Factorial {
+ public static void main (String args[]){
+ int i,fact=1];
+ int numer=5;
+ for (i=1;i<numer;i++){
+	fact=fact*i:
+ }
+ System.out.println("Factorial of"+number+"is: "+fact);
+. }
+}
